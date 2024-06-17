b75daf1d467086bddfaacae4851599062ac58736
NextGenEng <58440325+THOR300@users.noreply.github.com>
2024-06-17 17:13:42 +0100

    Update repo_info.py to revert local file creation.

--- flows/repo_info.py
+++ flows/repo_info.py
@@ -9,9 +9,6 @@ def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
     response = httpx.get(url)
     response.raise_for_status()
     repo = response.json()
-    print("Creating a local file to prove that the flow ran successfully and locally 📁")
-    with open("repo.json", "w") as f:
-        json.dump(repo, f)
     print(f"{repo_name} repository statistics 🤓:")
     print(f"Stars 🌠 : {repo['stargazers_count']}")
     print(f"Forks 🍴 : {repo['forks_count']}")
