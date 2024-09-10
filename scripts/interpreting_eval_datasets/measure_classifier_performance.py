cdb85e9ac521f0af921ef8b66ec52c2eb5844a39
harrisonpim <hello@harrisonpim.com>
2024-09-10 10:36:32 +0100

    run metrics calculations for multiple jaccard thresholds, and save results to disk

--- scripts/interpreting_eval_datasets/measure_classifier_performance.py
+++ scripts/interpreting_eval_datasets/measure_classifier_performance.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Annotated
 
+import pandas as pd
 from rich import box
 from rich.console import Console
 from rich.table import Table
@@ -16,26 +17,12 @@ console = Console(highlight=False)
 
 
 app = Typer()
+thresholds = [0, 0.1, 0.5, 0.9, 1]
 
 
 @app.command()
 def main(
     config_path: Annotated[Path, Option(..., help="Path to the sampling config")],
-    threshold: Annotated[
-        float,
-        Option(
-            ...,
-            help=(
-                "Jaccard similarity threshold for filtering. A value of 0.5 means that "
-                "two spans are considered similar if one span shares at least half of "
-                "its tokens with the other span. 1 corresponds to an exact match, "
-                "while 0 corresponds to no overlap. Use a very small value to allow "
-                "for matches with very little overlap."
-            ),
-            min=0,
-            max=1,
-        ),
-    ] = 0.5,
     verbose: Annotated[
         bool,
         Option(
@@ -51,8 +38,6 @@ def main(
     config = SamplingConfig.load(config_path)
     console.log("✅ Config loaded")
 
-    console.log(f"🔍 Using a jaccard similarity threshold of {threshold} for equality")
-
     labelled_passages_dir = processed_data_dir / "labelled_passages"
 
     if not labelled_passages_dir.exists():
@@ -60,18 +45,8 @@ def main(
             "Labelled passages data doesn't exist. Run save_labelled_passages_from_argilla.py first"
         )
 
-    table = Table(box=box.SIMPLE_HEAVY)
-    table.add_column("Preferred label", style="cyan", justify="right")
-    table.add_column("Wikibase ID", style="cyan")
-    table.add_column("Precision", style="magenta")
-    table.add_column("Recall", style="magenta")
-    table.add_column("F1 Score", style="magenta")
-
     for wikibase_id in config.wikibase_ids:
-        if verbose:
-            console.log(
-                f"📚 Evaluating classifier performance for concept [bold]{wikibase_id}[/bold]"
-            )
+        results = []
         # Load the labelled passages for the concept
         labelled_passages_path = labelled_passages_dir / wikibase_id / "agreements.json"
         human_labelled_passages = [
@@ -89,70 +64,86 @@ def main(
                     spans=classifier.predict(labelled_passage.text),
                 )
             )
-
-        true_positives = 0
-        false_positives = 0
-        false_negatives = 0
-        for human_labelled_passage, model_labelled_passage in zip(
-            human_labelled_passages, model_labelled_passages
-        ):
-            passage_level_agreement = True
-            for human_span in human_labelled_passage.spans:
-                found = False
-                for model_span in model_labelled_passage.spans:
-                    if jaccard_similarity(human_span, model_span) >= threshold:
-                        found = True
-                        true_positives += 1
-                        break
-                if not found:
-                    passage_level_agreement = False
-                    false_negatives += 1
-                    break
-
-            for model_span in model_labelled_passage.spans:
-                found = False
+        for threshold in thresholds:
+            true_positives = 0
+            false_positives = 0
+            false_negatives = 0
+            for human_labelled_passage, model_labelled_passage in zip(
+                human_labelled_passages, model_labelled_passages
+            ):
                 for human_span in human_labelled_passage.spans:
-                    if jaccard_similarity(model_span, human_span) >= threshold:
-                        found = True
+                    found = False
+                    for model_span in model_labelled_passage.spans:
+                        if jaccard_similarity(human_span, model_span) >= threshold:
+                            found = True
+                            true_positives += 1
+                            break
+                    if not found:
+                        false_negatives += 1
                         break
-                if not found:
-                    passage_level_agreement = False
-                    false_positives += 1
-                    break
 
-            if verbose:
-                console.log("Human-labelled passage:", style="white bold")
-                console.log(human_labelled_passage.get_highlighted_text())
-                console.log("Model-labelled passage:", style="white bold")
-                console.log(model_labelled_passage.get_highlighted_text())
-                console.log(
-                    f"Passage-level agreement: {'✅' if passage_level_agreement else '❌'}",
-                    style="white bold",
-                    end="\n\n",
-                )
+                for model_span in model_labelled_passage.spans:
+                    found = False
+                    for human_span in human_labelled_passage.spans:
+                        if jaccard_similarity(model_span, human_span) >= threshold:
+                            found = True
+                            break
+                    if not found:
+                        false_positives += 1
+                        break
 
-        try:
-            precision = true_positives / (true_positives + false_positives)
-        except ZeroDivisionError:
-            precision = 0
-        try:
-            recall = true_positives / (true_positives + false_negatives)
-        except ZeroDivisionError:
-            recall = 0
-        try:
-            f1_score = 2 * (precision * recall) / (precision + recall)
-        except ZeroDivisionError:
-            f1_score = 0
+            try:
+                precision = true_positives / (true_positives + false_positives)
+            except ZeroDivisionError:
+                precision = 0
+            try:
+                recall = true_positives / (true_positives + false_negatives)
+            except ZeroDivisionError:
+                recall = 0
+            try:
+                f1_score = 2 * (precision * recall) / (precision + recall)
+            except ZeroDivisionError:
+                f1_score = 0
 
-        table.add_row(
-            f"[link=https://climatepolicyradar.wikibase.cloud/wiki/Item:{wikibase_id}]{classifier.concept.preferred_label}[/link]",
-            f"[link=https://climatepolicyradar.wikibase.cloud/wiki/Item:{wikibase_id}]{wikibase_id}[/link]",
-            f"{precision:.2f}",
-            f"{recall:.2f}",
-            f"{f1_score:.2f}",
+            results.append(
+                {
+                    "wikibase_id": wikibase_id,
+                    "threshold": threshold,
+                    "precision": precision,
+                    "recall": recall,
+                    "f1_score": f1_score,
+                }
+            )
+
+        results_path = (
+            processed_data_dir / "classifier_performance" / f"{wikibase_id}.json"
         )
+        results_path.parent.mkdir(parents=True, exist_ok=True)
 
-    console.print(table)
+        results = pd.DataFrame(results)
+        results.to_json(results_path, orient="records")
+        if verbose:
+            table = Table(
+                box=box.SIMPLE,
+                show_header=True,
+                title=f"{classifier.concept.preferred_label} ({wikibase_id})",
+                title_justify="left",
+                title_style="bold",
+            )
+            table.add_column("threshold", style="magenta", width=12)
+            table.add_column("precision", style="magenta", width=12)
+            table.add_column("recall", style="magenta", width=12)
+            table.add_column("f1_score", style="magenta", width=12)
+            for _, row in results.iterrows():
+                table.add_row(
+                    str(row["threshold"]),
+                    f"{row['precision']:.2f}",
+                    f"{row['recall']:.2f}",
+                    f"{row['f1_score']:.2f}",
+                )
+            console.print(table)
+
+    console.log(f"📝 Wrote results to {results_path.parent}")
 
 
 if __name__ == "__main__":
