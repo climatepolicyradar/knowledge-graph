277e8e5071084e1ee99f884d9b0aa4553792395d
harrisonpim <hello@harrisonpim.com>
2024-10-03 12:02:13 +0100

    remove the negative labels constraint

--- src/classifier/embedding.py
+++ src/classifier/embedding.py
@@ -1,7 +1,6 @@
 from sentence_transformers import SentenceTransformer
 
 from src.classifier.classifier import Classifier
-from src.classifier.keyword import KeywordClassifier
 from src.concept import Concept
 from src.span import Span
 
@@ -33,15 +32,6 @@ class EmbeddingClassifier(Classifier):
         self.concept_text = ", ".join(self.concept.all_labels)
         self.concept_embedding = self.embedding_model.encode(self.concept_text)
 
-        if self.concept.negative_labels:
-            # if the concept has negative labels, they should still be respected, so
-            # we'll use an internal regex-based classifier to filter out negative cases
-            negative_concept = Concept(
-                preferred_label=self.concept.negative_labels[0],
-                alternative_labels=self.concept.negative_labels[1:],
-            )
-            self.negative_keyword_classifier = KeywordClassifier(negative_concept)
-
     def predict(self, text: str, threshold: float = 0.65) -> list[Span]:
         """
         Predict whether the supplied text contains an instance of the concept.
@@ -65,13 +55,4 @@ class EmbeddingClassifier(Classifier):
                     labellers=[str(self)],
                 )
             ]
-
-        # make sure that the embedding classifier still respects negative labels
-        if hasattr(self, "negative_keyword_classifier"):
-            spans = [
-                span
-                for span in spans
-                if not self.negative_keyword_classifier.predict(span.text)
-            ]
-
         return spans
