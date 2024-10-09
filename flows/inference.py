db42911c72e10d07264045863ce86659c2416fb7
olaughter <51889566+olaughter@users.noreply.github.com>
2024-10-09 17:33:59 +0100

    Feature/pla 196 create document model inference (#52)
    * Add parser output fixtures
    
    * Add fishing classifier, to be used in tests
    
    * Add initial inference flow
    
    Sets up prefect flow logic to take parser outputs, and run concepts over
    the text passages.
    
    * Change target bucket prefix to "labelled_passages"
    
    * Rewrite `determine_document_ids` docstring for clarity
    
    * Fix tense for classifier logging
    
    * Update infer function naming
    
    * Fix failing test
    
    * Be explicit for failing on bad document data
    
    * move to string functionality into module
    
    * add missing test for doc_passages
    
    * Add placeholder parameter for classifier selection
    
    This is out of scope of this PR, and will need revisiting, here the
    initial implementation is tidied to make it easier to hook in later

---
+++ flows/inference.py
@@ -0,0 +1,204 @@
+import json
+import os
+from dataclasses import dataclass
+from io import BytesIO
+from pathlib import Path
+
+import boto3
+from cpr_sdk.parser_models import BaseParserOutput
+from prefect import flow, task
+
+from src.classifier import Classifier
+from src.identifiers import WikibaseID
+from src.labelled_passage import LabelledPassage
+from src.span import Span
+
+
+@dataclass()
+class Config:
+    """Settings used across flow runs"""
+
+    cache_bucket: str = os.environ.get("CACHE_BUCKET")
+    document_source_prefix: str = "embeddings_input"
+    document_target_prefix: str = "labelled_passages"
+    bucket_region: str = "eu-west-1"
+    local_classifier_dir: Path = Path("data") / "processed" / "classifiers"
+
+
+config = Config()
+
+
+def list_bucket_doc_ids() -> list[str]:
+    """Scan configured bucket and return all ids"""
+    s3 = boto3.client("s3", region_name=config.bucket_region)
+    paginator = s3.get_paginator("list_objects_v2")
+    page_iterator = paginator.paginate(
+        Bucket=config.cache_bucket,
+        Prefix=config.document_source_prefix,
+    )
+    doc_ids = []
+
+    for p in page_iterator:
+        if "Contents" in p:
+            for o in p["Contents"]:
+                doc_id = Path(o["Key"]).stem
+                doc_ids.append(doc_id)
+    return doc_ids
+
+
+def determine_document_ids(
+    requested_document_ids: list[str], current_bucket_ids: list[str]
+) -> list[str]:
+    """
+    Confirm chosen document ids or default to all if not specified.
+
+    Compares the requested_document_ids to what actually exists in the bucket.
+    If a document id has been requested but does not exist this will raise a ValueError
+    If no document id were requested, this will instead return the current_bucket_ids.
+    """
+    if requested_document_ids is None:
+        return current_bucket_ids
+
+    missing_from_bucket = list(set(requested_document_ids) - set(current_bucket_ids))
+    if len(missing_from_bucket) > 0:
+        raise ValueError(
+            f"Requested document_ids not found in bucket: {missing_from_bucket}"
+        )
+
+    return requested_document_ids
+
+
+def load_classifier(wikibase_id: WikibaseID, alias: str) -> Classifier:
+    """
+    Loads a classifier into memory
+
+    If the classifier is available locally, this will be used. Otherwise the
+    classifier will be downloaded from W&B (Once implemented)
+    """
+    local_classifier_path: Path = config.local_classifier_dir / wikibase_id
+
+    if not local_classifier_path.exists():
+        raise NotImplementedError("Still need to add W&B download")
+
+    classifier = Classifier.load(local_classifier_path)
+
+    return classifier
+
+
+def load_document(document_id: str) -> BaseParserOutput:
+    """Downloads and opens a parser output based on a document id"""
+    s3 = boto3.client("s3", region_name=config.bucket_region)
+
+    file_key = os.path.join(config.document_source_prefix, f"{document_id}.json")
+
+    response = s3.get_object(Bucket=config.cache_bucket, Key=file_key)
+    content = response["Body"].read().decode("utf-8")
+    document = BaseParserOutput.model_validate_json(content)
+    return document
+
+
+def stringify(text: list[str]) -> str:
+    return " ".join([line.strip() for line in text])
+
+
+def document_passages(document: BaseParserOutput):
+    """Yields the text block irrespective of content type"""
+    match document.document_content_type:
+        case "application/pdf":
+            text_blocks = document.pdf_data.text_blocks
+        case "text/html":
+            text_blocks = document.html_data.text_blocks
+        case _:
+            raise ValueError(
+                f"Invalid document content type: {document.document_content_type}, for "
+                f"document: {document.document_id}"
+            )
+    for text_block in text_blocks:
+        yield stringify(text_block.text), text_block.text_block_id
+
+
+def store_labels(labels: list[LabelledPassage], document_id: str, classifier_id: str):
+    key = os.path.join(
+        config.document_target_prefix, f"{document_id}.{classifier_id}.json"
+    )
+
+    data = [label.model_dump() for label in labels]
+    body = BytesIO(json.dumps(data).encode("utf-8"))
+
+    s3 = boto3.client("s3", region_name=config.bucket_region)
+    s3.put_object(
+        Bucket=config.cache_bucket, Key=key, Body=body, ContentType="application/json"
+    )
+
+
+@task()
+def text_block_inference(
+    classifier: Classifier, block_id: str, text: str
+) -> LabelledPassage:
+    """Runs predict on a single text block"""
+    spans: list[Span] = classifier.predict(text)
+    labelled_passage = LabelledPassage(
+        id=block_id,
+        text=text,
+        spans=spans,
+    )
+    return labelled_passage
+
+
+def determine_classifier_ids(
+    classifier_spec: list[tuple[WikibaseID, str]],
+) -> list[WikibaseID]:
+    """
+    To implement.
+
+    A check that requested classifiers exist, or return all the latest classifiers
+    """
+    return [(WikibaseID("Q788"), "latest")]
+
+
+@flow(log_prints=True)
+def classifier_inference(
+    document_ids: list[str] = None,
+    classifier_spec: list[tuple[WikibaseID, str]] = None,
+):
+    """
+    Flow to run inference on documents within a bucket prefix
+
+    Default behaviour is to run on everything, pass document_ids to limit to specific
+    files.
+
+    Iterates: classifiers > documents > passages. Loading output into s3
+    """
+    print(f"Running with config: {config}")
+
+    current_bucket_ids = list_bucket_doc_ids()
+    validated_document_ids = determine_document_ids(
+        requested_document_ids=document_ids, current_bucket_ids=current_bucket_ids
+    )
+    classifier_spec = determine_classifier_ids(classifier_spec)
+
+    for wikibase_id, classifier_alias in classifier_spec:
+        print(
+            f"Loading classifier with id: {wikibase_id}, and alias: {classifier_alias}"
+        )
+        classifier = load_classifier(wikibase_id, classifier_alias)
+        for document_id in validated_document_ids:
+            print(f"Loading document with id {document_id}")
+            document = load_document(document_id)
+
+            doc_labels = []
+            for text, block_id in document_passages(document):
+                labelled_passage = text_block_inference(
+                    classifier=classifier, block_id=block_id, text=text
+                )
+                doc_labels.append(labelled_passage)
+
+            store_labels(
+                labels=doc_labels,
+                document_id=document_id,
+                classifier_id=wikibase_id,
+            )
+
+
+if __name__ == "__main__":
+    classifier_inference()
---
+++ tests/flows/conftest.py
@@ -0,0 +1,130 @@
+import os
+from io import BytesIO
+from pathlib import Path
+from typing import Any, Generator
+from unittest.mock import patch
+
+import boto3
+import pytest
+from cpr_sdk.parser_models import (
+    BaseParserOutput,
+    BlockType,
+    HTMLData,
+    HTMLTextBlock,
+    PDFData,
+    PDFTextBlock,
+)
+from moto import mock_aws
+
+from flows.inference import config
+from src.identifiers import WikibaseID
+
+FIXTURE_DIR = Path(__file__).resolve().parent / "fixtures"
+
+
+@pytest.fixture(scope="function")
+def mock_aws_creds():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+
+
+@pytest.fixture
+def mock_s3_client(mock_aws_creds) -> Generator[str, Any, Any]:
+    with mock_aws():
+        yield boto3.client("s3")
+
+
+@pytest.fixture
+def mock_bucket(mock_aws_creds, mock_s3_client) -> Generator[str, Any, Any]:
+    test_bucket_name = "test_bucket"
+    with patch.object(config, "cache_bucket", new=test_bucket_name):
+        mock_s3_client.create_bucket(
+            Bucket=test_bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-1"},
+        )
+        yield test_bucket_name
+
+
+def load_fixture(file_name):
+    fixture_path = FIXTURE_DIR / file_name
+    with open(fixture_path) as f:
+        return f.read()
+
+
+@pytest.fixture
+def mock_bucket_documents(mock_s3_client, mock_bucket):
+    fixture_files = ["PDF.document.0.1.json", "HTML.document.0.1.json"]
+    for file_name in fixture_files:
+        data = load_fixture(file_name)
+        body = BytesIO(data.encode("utf-8"))
+        key = os.path.join("embeddings_input", file_name)
+        mock_s3_client.put_object(
+            Bucket=mock_bucket, Key=key, Body=body, ContentType="application/json"
+        )
+    yield fixture_files
+
+
+@pytest.fixture
+def mock_classifiers_dir():
+    mock_dir = Path(FIXTURE_DIR) / "classifiers"
+    with patch.object(config, "local_classifier_dir", new=mock_dir):
+        yield mock_dir
+
+
+@pytest.fixture
+def local_classifier_id(mock_classifiers_dir):
+    classifier_id = WikibaseID("Q788")
+    full_path = mock_classifiers_dir / classifier_id
+    assert full_path.exists()
+    yield classifier_id
+
+
+@pytest.fixture
+def parser_output():
+    yield BaseParserOutput(
+        document_id="test id",
+        document_metadata={},
+        document_name="test name",
+        document_slug="test slug",
+        document_description="test description",
+    )
+
+
+@pytest.fixture
+def parser_output_html(parser_output):
+    parser_output.document_content_type = "text/html"
+    parser_output.html_data = HTMLData(
+        has_valid_text=True,
+        text_blocks=[
+            HTMLTextBlock(
+                text=["test html text"],
+                text_block_id="1",
+            )
+        ],
+    )
+    yield parser_output
+
+
+@pytest.fixture
+def parser_output_pdf(parser_output):
+    # When the content type is pdf
+    parser_output.document_content_type = "application/pdf"
+    parser_output.html_data = None
+    parser_output.pdf_data = PDFData(
+        page_metadata=[],
+        md5sum="",
+        text_blocks=[
+            PDFTextBlock(
+                text=["test pdf text"],
+                text_block_id="2",
+                page_number=1,
+                coords=[],
+                type=BlockType.TEXT,
+                type_confidence=0.5,
+            )
+        ],
+    )
+    yield parser_output
