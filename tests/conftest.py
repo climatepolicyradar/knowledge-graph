4caf70c858ca4078ed77ae1294535c0647fab03a
Jesse Claven <jesse.claven@me.com>
2024-10-23 11:15:11 +0100

    refactor(evaluate): Use smaller functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,40 @@
+import json
 import os
+from pathlib import Path
 
+import pandas as pd
 import pytest
+
+from src.classifier import Classifier
+from src.concept import Concept
 
 
 @pytest.fixture(scope="function")
 def aws_credentials():
-    """Mocked AWS Credentials for moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
+
+
+@pytest.fixture
+def metrics_df() -> pd.DataFrame:
+    fixture_path = Path(
+        "tests/fixtures/data/processed/classifiers_performance/Q787.json"
+    )
+    with open(fixture_path) as f:
+        data = json.load(f)
+
+    return pd.DataFrame(data)
+
+
+@pytest.fixture
+def concept() -> Concept:
+    fixture_path = Path("tests/fixtures/data/processed/concepts/Q787.json")
+    return Concept.load(fixture_path)
+
+
+@pytest.fixture
+def classifier() -> Classifier:
+    fixture_path = Path("tests/fixtures/data/processed/classifiers/Q787")
+    return Classifier.load(fixture_path)
