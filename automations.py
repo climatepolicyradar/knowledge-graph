d827e7c84ccfaf4cd14cb36d34087eb80c00666e
Jesse Claven <jesse.claven@me.com>
2024-11-12 21:53:14 +0000

    feat(automations): Add trigger for inference→indexing
    This is a common trigger, so I refactored it to be shared between the
    2 automations (per AWS environment).
    
    **Testing**
    
    Creation:
    
    ```
    $ AWS_ENV=labs poetry run python -m automations
    
    Loading Deployment: knowledge-graph-classifier-inference-labs
    21:49:26.924 | INFO    | __main__ - Loading Deployment: knowledge-graph-classifier-inference-labs
    Loaded Deployment: name=knowledge-graph-classifier-inference-labs, flow_id=bf3da105-202c-4cfa-94cc-23f22427c3d8
    21:49:27.359 | INFO    | __main__ - Loaded Deployment: name=knowledge-graph-classifier-inference-labs, flow_id=bf3da105-202c-4cfa-94cc-23f22427c3d8
    Loading Deployment: knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs
    21:49:27.359 | INFO    | __main__ - Loading Deployment: knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs
    Loaded Deployment: name=knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs, flow_id=c26d97f9-6d6b-47d9-80ee-861bbb44e213
    21:49:27.495 | INFO    | __main__ - Loaded Deployment: name=knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs, flow_id=c26d97f9-6d6b-47d9-80ee-861bbb44e213
    Automation doesn't exist already, creating it
    21:49:27.640 | INFO    | __main__ - Automation doesn't exist already, creating it
    Created automation with id='38c22a90-45ab-4888-8c91-b992d5aaa724'
    21:49:27.971 | INFO    | __main__ - Created automation with id='38c22a90-45ab-4888-8c91-b992d5aaa724'
    ```
    
    Update:
    
    ```
    $ AWS_ENV=labs poetry run python -m automations
    
    Loading Deployment: knowledge-graph-classifier-inference-labs
    21:51:48.220 | INFO    | __main__ - Loading Deployment: knowledge-graph-classifier-inference-labs
    Loaded Deployment: name=knowledge-graph-classifier-inference-labs, flow_id=bf3da105-202c-4cfa-94cc-23f22427c3d8
    21:51:48.392 | INFO    | __main__ - Loaded Deployment: name=knowledge-graph-classifier-inference-labs, flow_id=bf3da105-202c-4cfa-94cc-23f22427c3d8
    Loading Deployment: knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs
    21:51:48.392 | INFO    | __main__ - Loading Deployment: knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs
    Loaded Deployment: name=knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs, flow_id=c26d97f9-6d6b-47d9-80ee-861bbb44e213
    21:51:48.551 | INFO    | __main__ - Loaded Deployment: name=knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs, flow_id=c26d97f9-6d6b-47d9-80ee-861bbb44e213
    Automation exists already, updating it
    21:51:48.692 | INFO    | __main__ - Automation exists already, updating it
    Read automation with id='38c22a90-45ab-4888-8c91-b992d5aaa724', name='trigger-knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs'
    21:51:48.693 | INFO    | __main__ - Read automation with id='38c22a90-45ab-4888-8c91-b992d5aaa724', name='trigger-knowledge-graph-index-labelled-passages-from-s3-to-vespa-labs'
    Updated automation
    21:51:48.925 | INFO    | __main__ - Updated automation
    ```
    
    Result:
    https://app.prefect.cloud/account/4b1558a0-3c61-4849-8b18-3e97e0516d78/workspace/1753b4f0-6221-4f6a-9233-b146518b4545/automations/automation/38c22a90-45ab-4888-8c91-b992d5aaa724
    
    FIXES PLA-287

--- automations.py
+++ automations.py
@@ -1,4 +1,4 @@
-"""Create automations in Prefect Cloud for running classifier inference."""
+"""Create automations in Prefect Cloud."""
 
 import asyncio
 import logging
@@ -12,8 +12,11 @@ from prefect.client.schemas.objects import Deployment
 from prefect.events.actions import RunDeployment
 from prefect.exceptions import ObjectNotFound
 
+from flows.index import index_labelled_passages_from_s3_to_vespa
 from flows.inference import classifier_inference
 from scripts.cloud import AwsEnv
+
+PROJECT_NAME = "knowledge-graph"
 
 # Create logger
 logger = logging.getLogger(__name__)
@@ -59,7 +62,7 @@ async def update_automation(client, automation_id: UUID, automation):
 
     # Transform these types to be serialisable
     json["trigger"]["within"] = int(json["trigger"]["within"].total_seconds())
-    # This lens matches the structure returned by `prime()`.
+    # This lens matches the structure returned in the target automation.
     json["actions"][0]["deployment_id"] = str(json["actions"][0]["deployment_id"])
 
     response = await client._client.put(
@@ -69,24 +72,47 @@ async def update_automation(client, automation_id: UUID, automation):
     response.raise_for_status
 
 
-def prime(
-    navigator_data_s3_backup_deployment: Deployment,
-    classifier_inference_deployment: Deployment,
-    aws_env: str,
+def create_target_automation(
+    a_flow_name: str,
+    a_deployment: Deployment,
+    b_deployment: Deployment,
+    parameters: dict,
 ) -> automations.Automation:
-    """Return a new copy of the target Automation."""
+    """
+    Create a copy of the `Automation` that triggers another `Deployment`.
+
+    This is the "target" in the sense that it's used for creating the
+    `Automation` if it doesn't exist yet or the `Automation` has changed
+    and we want to update the existing `Automation`s in Prefect Cloud to
+    reflect the updated approach.
+
+    The `Automation` will trigger `Deployment` B to run after
+    `Deployment` A has completed successfully. The ~Automation`
+    watches for completion events from `Flow` A `Run`s and
+    specifically from `Deployment` A, then launches `Deployment` B
+    with the provided parameters.
+
+    Args:
+        a_flow_name: The name of the `Flow` that needs to complete first
+        a_deployment: The `Deployment` of `Flow` A that needs to complete
+        b_deployment: The `Deployment` to trigger after A completes
+        parameters: Parameters to pass to `Deployment` B when it's triggered
+
+    Returns:
+        A Prefect Automation object configured to trigger B after A completes
+    """
     return automations.Automation(
-        name=f"trigger-{classifier_inference_deployment.name}",
+        name=f"trigger-{b_deployment.name}",
         description="Start concept store inference with classifiers.",
         trigger=automations.EventTrigger(
             match={
                 "prefect.resource.id": "prefect.flow-run.*",
-                "prefect.resource.name": ["navigator-data-s3-backup"],
+                "prefect.resource.name": [a_flow_name],
             },
             match_related=automations.ResourceSpecification(
                 __root__={
                     # This is the Deployment, for the specific AWS environment.
-                    "prefect.resource.id": str(navigator_data_s3_backup_deployment.id),
+                    "prefect.resource.id": str(a_deployment.id),
                 }
             ),
             posture="Proactive",
@@ -100,39 +126,37 @@ def prime(
                 #
                 # [1] https://github.com/PrefectHQ/prefect/blob/ab964c1c4b52fd9ae61bc8d816505ac89df7a8f8/src/prefect/events/actions.py#L32
                 source="selected",
-                deployment_id=classifier_inference_deployment.id,
-                parameters={"use_new_and_updated": True},
+                deployment_id=b_deployment.id,
+                parameters=parameters,
             ),
         ],
     )
 
 
-async def main() -> None:
-    """Create or update the automation for triggering inference."""
-    aws_env = os.getenv("AWS_ENV")
-    if aws_env is None or aws_env == "":
-        raise ValueError("AWS_ENV is missing")
+def flow_deployment_names_id(flow_name, deployment_name):
+    return f"{flow_name}/{deployment_name}"
 
-    project_name = "knowledge-graph"
 
+async def a_triggers_b(
+    a_flow_name: str,
+    a_deployment_name: str,
+    b_flow_name: str,
+    b_deployment_name: str,
+    b_parameters: dict,
+    ignore: list[AwsEnv],
+    aws_env: AwsEnv,
+) -> None:
+    """Automation to after Deployment A completes trigger Deployment B."""
     client = get_client()
-
-    navigator_data_s3_backup_flow_name = "navigator-data-s3-backup"
-    navigator_data_s3_backup_deployment_name = (
-        f"{navigator_data_s3_backup_flow_name}/"
-        f"navigator-data-s3-backup-pipeline-cache-{aws_env}"
-    )
 
     logger.info(
         "Loading Deployment: %s",
-        navigator_data_s3_backup_deployment_name,
+        a_deployment_name,
     )
 
-    ignore = [AwsEnv.labs.value]
-
     try:
-        navigator_data_s3_backup_deployment = await client.read_deployment_by_name(
-            name=navigator_data_s3_backup_deployment_name
+        a_deployment = await client.read_deployment_by_name(
+            name=flow_deployment_names_id(a_flow_name, a_deployment_name)
         )
     except ObjectNotFound:
         if aws_env in ignore:
@@ -145,44 +169,39 @@ async def main() -> None:
 
     logger.info(
         "Loaded Deployment: name=%s, flow_id=%s",
-        navigator_data_s3_backup_deployment.name,
-        navigator_data_s3_backup_deployment.flow_id,
+        a_deployment.name,
+        a_deployment.flow_id,
     )
 
-    classifier_inference_flow_name = classifier_inference.name
-    classifier_inference_deployment_name = (
-        f"{classifier_inference_flow_name}/"
-        f"{project_name}-{classifier_inference.name}-{aws_env}"
-    )
+    logger.info("Loading Deployment: %s", b_deployment_name)
 
-    logger.info("Loading Deployment: %s", classifier_inference_deployment_name)
-
-    classifier_inference_deployment = await client.read_deployment_by_name(
-        name=classifier_inference_deployment_name
+    b_deployment = await client.read_deployment_by_name(
+        name=flow_deployment_names_id(b_flow_name, b_deployment_name)
     )
 
     logger.info(
         "Loaded Deployment: name=%s, flow_id=%s",
-        classifier_inference_deployment.name,
-        classifier_inference_deployment.flow_id,
+        b_deployment.name,
+        b_deployment.flow_id,
     )
 
-    original = prime(
-        navigator_data_s3_backup_deployment,
-        classifier_inference_deployment,
-        aws_env,
+    target = create_target_automation(
+        a_flow_name=a_flow_name,
+        a_deployment=a_deployment,
+        b_deployment=b_deployment,
+        parameters=b_parameters,
     )
 
     automations = await read_automations_by_name(
         client=client,
-        name=original.name,
+        name=target.name,
     )
 
     match len(automations):
         case 0:
             logger.info("Automation doesn't exist already, creating it")
 
-            automation_id = await client.create_automation(automation=original)
+            automation_id = await client.create_automation(automation=target)
 
             logger.info("Created automation with id='%s'", automation_id)
         case 1:
@@ -198,7 +217,7 @@ async def main() -> None:
             await update_automation(
                 client=client,
                 automation_id=UUID(automation["id"]),
-                automation=original,
+                automation=target,
             )
 
             logger.info("Updated automation")
@@ -206,8 +225,33 @@ async def main() -> None:
             names = [auto["name"] for auto in automations]
 
             raise ValueError(
-                f"Found multiple automations with name {original.name}: {names}"
+                f"Found multiple automations with name {target.name}: {names}"
             )
+
+
+async def main() -> None:
+    """Create or update the automation for triggering inference."""
+    aws_env = AwsEnv(os.getenv("AWS_ENV"))
+
+    await a_triggers_b(
+        a_flow_name="navigator-data-s3-backup",
+        a_deployment_name=f"navigator-data-s3-backup-pipeline-cache-{aws_env}",
+        b_flow_name=classifier_inference.name,
+        b_deployment_name=f"{PROJECT_NAME}-{classifier_inference.name}-{aws_env}",
+        b_parameters={"use_new_and_updated": True},
+        aws_env=aws_env,
+        ignore=[AwsEnv.labs],
+    )
+
+    await a_triggers_b(
+        a_flow_name=classifier_inference.name,
+        a_deployment_name=f"{PROJECT_NAME}-{classifier_inference.name}-{aws_env}",
+        b_flow_name=index_labelled_passages_from_s3_to_vespa.name,
+        b_deployment_name=f"{PROJECT_NAME}-{index_labelled_passages_from_s3_to_vespa.name}-{aws_env}",
+        b_parameters={},
+        aws_env=aws_env,
+        ignore=[],
+    )
 
 
 if __name__ == "__main__":
