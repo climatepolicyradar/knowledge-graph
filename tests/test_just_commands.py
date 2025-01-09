cd077db4d229fa154b9903bada9053346a414eda
Kalyan Dutia <kalyan.dutia@gmail.com>
2025-01-09 13:59:16 +0000

    skip justfile test

--- tests/test_just_commands.py
+++ tests/test_just_commands.py
@@ -1,3 +1,9 @@
+import pytest
+
+
+@pytest.mark.skip(
+    reason="Relies on the full feather file which is not pushed to the repo"
+)
 def test_analyse_classifier(
     run_just_command, concept_wikibase_id, tmp_path, monkeypatch
 ):
