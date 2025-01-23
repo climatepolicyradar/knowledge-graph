00dcc69fc6135fbb4fcc468568a03a705c3a0127
olaughter <fred@climatepolicyradar.org>
2025-01-23 15:10:27 +0000

    Fix identifying copy discrepencies
    The way we where comparing previosuly meant we'd compare wikibase ids
    against s3 paths. So they where all flagged as extras in s3! This way we
    actually compare the ids instead

--- tests/flows/test_wikibase_to_s3.py
+++ tests/flows/test_wikibase_to_s3.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 from botocore.exceptions import ClientError
 
@@ -26,15 +28,21 @@ def test_upload_to_s3(
 def test_list_s3_concepts(mock_cdn_concepts, test_wikibase_to_s3_config):
     results = list_s3_concepts(config=test_wikibase_to_s3_config)
     assert set(results) == {
-        "concepts/Q10.json",
-        "concepts/Q20.json",
-        "concepts/Q30.json",
+        "Q10",
+        "Q20",
+        "Q30",
     }
 
 
 def test_wikibase_to_s3(
-    MockedWikibaseSession, mock_cdn_concepts, test_wikibase_to_s3_config, mock_s3_client
+    caplog,
+    MockedWikibaseSession,
+    mock_cdn_concepts,
+    test_wikibase_to_s3_config,
+    mock_s3_client,
 ):
+    caplog.set_level(logging.INFO)
+
     def get_concept_from_s3(wikibase_id):
         result = mock_s3_client.get_object(
             Bucket=test_wikibase_to_s3_config.cdn_bucket_name,
@@ -44,22 +52,24 @@ def test_wikibase_to_s3(
 
     # Concept count before running from test setup concepts
     start = list_s3_concepts(config=test_wikibase_to_s3_config)
-    assert set(start) == {"concepts/Q10.json", "concepts/Q20.json", "concepts/Q30.json"}
+    assert set(start) == {"Q10", "Q20", "Q30"}
     before_overwrite = get_concept_from_s3("Q10")
 
     # Run once adding new concepts from store
+    assert "Extras: ['Q20', 'Q30']" not in caplog.text
     wikibase_to_s3.fn(config=test_wikibase_to_s3_config)
     run_with_new = list_s3_concepts(config=test_wikibase_to_s3_config)
     assert set(run_with_new) == set(
         start
         + [
-            "concepts/Q10.json",
-            "concepts/Q100.json",
-            "concepts/Q1000.json",
-            "concepts/Q1001.json",
-            "concepts/Q1002.json",
+            "Q10",
+            "Q100",
+            "Q1000",
+            "Q1001",
+            "Q1002",
         ]
     )
+    assert "Extras: ['Q20', 'Q30']" in caplog.text
 
     # Rerun again, without new documents in the store
     wikibase_to_s3.fn(config=test_wikibase_to_s3_config)
