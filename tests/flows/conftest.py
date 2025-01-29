b83d935d15396a56f24267f61bbddbcc271a8136
olaughter <fred@climatepolicyradar.org>
2025-01-29 10:17:00 +0000

    Add reusable slack notifier for prefect flows
    Intended to be work for any flow and be called from flow hooks

--- tests/flows/conftest.py
+++ tests/flows/conftest.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from io import BytesIO
 from pathlib import Path
 from typing import Any, Generator
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, Mock, patch
 
 import boto3
 import pytest
@@ -20,6 +20,7 @@ from cpr_sdk.parser_models import (
 )
 from cpr_sdk.search_adaptors import VespaSearchAdapter
 from moto import mock_aws
+from prefect import Flow, State
 from pydantic import SecretStr
 from requests.exceptions import ConnectionError
 from vespa.application import Vespa
@@ -511,3 +512,34 @@ def mock_cdn_concepts(
         )
 
     yield mock_concepts
+
+
+@pytest.fixture
+def mock_prefect_slack_webhook():
+    """Patch the SlackWebhook class to return a mock object."""
+    with patch("flows.utils.SlackWebhook") as mock_SlackWebhook:
+        mock_prefect_slack_block = MagicMock()
+        mock_SlackWebhook.load.return_value = mock_prefect_slack_block
+        yield mock_SlackWebhook, mock_prefect_slack_block
+
+
+@pytest.fixture
+def mock_flow():
+    """Mock Prefect flow object."""
+    mock_flow = MagicMock(spec=Flow)
+    mock_flow.name = "TestFlow"
+    yield mock_flow
+
+
+@pytest.fixture
+def mock_flow_run():
+    """Mock Prefect flow run object."""
+    mock_flow_run = MagicMock()
+    mock_flow_run.name = "TestFlowRun"
+    mock_flow_run.id = "test-flow-run-id"
+    mock_flow_run.state = MagicMock(spec=State)
+    mock_flow_run.state.name = "Completed"
+    mock_flow_run.state.message = "message"
+    mock_flow_run.state.timestamp = "2025-01-28T12:00:00+00:00"
+
+    yield mock_flow_run
