f807a7409eb05f264a7b749de746c29c6f3122d8
olaughter <fred@climatepolicyradar.org>
2025-02-04 12:03:59 +0000

    Add initial test for loading concept counts
    Tests the main subflow of the concept_counts indexing

--- tests/flows/test_count_family_document_concepts.py
+++ tests/flows/test_count_family_document_concepts.py
@@ -1,9 +1,14 @@
+import asyncio
 import json
 from io import BytesIO
 
 import pytest
+from cpr_sdk.search_adaptors import VespaSearchAdapter
 
-from flows.count_family_document_concepts import load_parse_concepts_counts
+from flows.count_family_document_concepts import (
+    load_parse_concepts_counts,
+    load_update_document_concepts_counts,
+)
 from src.concept import Concept
 from src.identifiers import WikibaseID
 
@@ -73,3 +78,39 @@ async def test_load_parse_concepts_counts_invalid_concept_key(
 
     with pytest.raises(ValueError, match="not enough values to unpack"):
         await load_parse_concepts_counts(document_object_uri)
+
+
+@pytest.mark.vespa
+@pytest.mark.asyncio
+async def test_load_update_document_concepts_counts(
+    local_vespa_search_adapter: VespaSearchAdapter,
+    document_passages_test_data_file_path: str,
+    vespa_app,
+    mock_bucket,
+    mock_bucket_concepts_counts,
+) -> None:
+    # Get specific id
+    BATCH_SIZE = 1
+    for doc_fixture_id in ["CCLW.executive.10014.4470", "CCLW.executive.4934.1571"]:
+        vespa_doc_id = f"id:doc_search:family_document::{doc_fixture_id}"
+        key = f"concepts_counts/Q761/v4/{doc_fixture_id}.json"
+        document_object_uris = [f"s3://{mock_bucket}/{key}"]
+
+        counts_before = local_vespa_search_adapter.get_by_id(
+            vespa_doc_id
+        ).concept_counts
+        if counts_before is not None:
+            pytest.fail(f"Fixture should not start with concept counts: {vespa_doc_id}")
+
+        task = load_update_document_concepts_counts(
+            document_import_id=doc_fixture_id,
+            document_object_uris=document_object_uris,
+            batch_size=1,
+            vespa_search_adapter=local_vespa_search_adapter,
+        )
+        loaded_counts = await asyncio.gather(task, return_exceptions=True)
+        assert len(loaded_counts) == BATCH_SIZE
+
+        counts_after = local_vespa_search_adapter.get_by_id(vespa_doc_id).concept_counts
+        assert counts_after == loaded_counts[0]
+        assert counts_after != counts_before
