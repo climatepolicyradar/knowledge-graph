0bf14ec23d365a1218baa73749bfa5f37a5aff69
mpjuhasz <juhaszmp@gmail.com>
2025-02-26 17:05:20 +0100

    added flow to automatically backup datasets to hf

---
+++ flows/data_backup.py
@@ -0,0 +1,41 @@
+"""
+Flow to deploy an automated backup of Argilla to Huggingface
+"""
+
+import os
+
+from cpr_sdk.ssm import get_aws_ssm_param
+from prefect import flow, task
+
+from src.huggingface import HuggingfaceSession
+from src.argilla_v2 import (
+    dataset_to_labelled_passages,
+    get_all_datasets,
+)
+
+
+@task
+def setup_environment():
+    """Set up required environment variables from SSM parameters."""
+
+    os.environ["ARGILLA_API_URL"] = get_aws_ssm_param("/Argilla/APIURL")
+    os.environ["ARGILLA_API_KEY"] = get_aws_ssm_param("/Argilla/APIKey")
+
+    os.environ["HF_TOKEN"] = get_aws_ssm_param("/Huggingface/Token")
+
+
+@flow
+def deploy_static_sites():
+    """Flow to deploy all our static sites."""
+    setup_environment()
+
+    hf_session = HuggingfaceSession()
+
+    datasets = get_all_datasets("knowledge-graph")
+    for dataset in datasets:
+        labelled_passages = dataset_to_labelled_passages(dataset)
+        hf_session.push(dataset.name, labelled_passages)
+
+
+if __name__ == "__main__":
+    deploy_static_sites()
