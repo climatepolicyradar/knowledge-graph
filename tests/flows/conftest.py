2dbc3b42d75aff928d1bf6e6b74165bbf34d26a3
Jesse Claven <jesse.claven@me.com>
2025-03-11 11:05:52 +0000

    feat: Add de-indexing pipeline
    This pipeline reads what's the state in S3, and then undoes that from
    Vespa (hence, the "de-index") and then removes the S3 objects as well.
    
    It uses the same interface as other pipelines, except that you must
    specify the classifiers specs, as we don't want to ever allow a full
    removal of all concepts.
    
    This approach avoids a full scan of Vespa, to remove a concept.
    
    **Resiliency on partial updates failures**
    
    De-indexing accounts for if updating a document passage fails. When removing a concept from the document passage fails, it'll still be counted, and written to S3.
    
    The usage here is intended to re-run the de-indexing pipeline again, for the concept(s) and family document(s), if the failure wasn't related to the pipeline itself. This could be a transient network failure. This very generously could be called idempotency.
    
    **I don't see changes for removing concepts from concepts counts**
    
    The concepts counts-related pipeline does a wholesale overwrite, after it's collected the concepts counts from S3. That means, a concepts counts object for concepts will be deleted in de-indexing, and thus won't be seen during the counting pipeline, and will be removed from the family document.
    
    **There's so much duplicated code now!**
    
    Yes, there is. Once it all is working, we can look at refactoring it to be DRY.
    
    TOWARDS PLA-481

--- tests/flows/conftest.py
+++ tests/flows/conftest.py
@@ -121,32 +121,22 @@ def vespa_app(
     print("\nSetting up Vespa connection...")
     app = Vespa(mock_vespa_credentials["VESPA_INSTANCE_URL"])
 
-    subprocess.run(["just", "vespa_feed_data"], capture_output=True, text=True)
+    subprocess.run(
+        ["just", "vespa_feed_data"],
+        check=True,
+        timeout=60,  # Seconds
+    )
 
     yield app  # This is where the test function will be executed
 
     # Teardown
     print("\nTearing down Vespa connection...")
-    delete_all_documents(app)
 
-
-def delete_all_documents(app):
-    print("Deleting all documents...")
-    print("Search weights...")
-    response = app.delete_all_docs(
-        content_cluster_name="family-document-passage", schema="search_weights"
+    subprocess.run(
+        ["just", "vespa_delete_data"],
+        check=True,
+        timeout=60,  # Seconds
     )
-    print(f"Delete response: {response}")
-    print("Family documents...")
-    response = app.delete_all_docs(
-        content_cluster_name="family-document-passage", schema="family_document"
-    )
-    print(f"Delete response: {response}")
-    print("Document passages...")
-    response = app.delete_all_docs(
-        content_cluster_name="family-document-passage", schema="document_passage"
-    )
-    print(f"Delete response: {response}")
 
 
 @pytest.fixture
@@ -356,7 +346,7 @@ def s3_prefix_mock_bucket_labelled_passages(
 @pytest.fixture
 def s3_prefix_labelled_passages() -> str:
     """Returns the s3 prefix for the concepts."""
-    return "labelled_passages/Q788/latest"
+    return "labelled_passages/Q788/v4"
 
 
 @pytest.fixture()
