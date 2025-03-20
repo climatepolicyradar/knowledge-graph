a2af1bb7eb3aba79a4fc9b229663b4870d4dffd8
mpjuhasz <juhaszmp@gmail.com>
2025-03-20 11:21:33 +0100

    formatting fixes

--- deployments.py
+++ deployments.py
@@ -18,6 +18,7 @@ from flows.count_family_document_concepts import (
     count_family_document_concepts,
     load_update_document_concepts_counts,
 )
+from flows.data_backup import data_backup
 from flows.deploy_static_sites import deploy_static_sites
 from flows.index import (
     index_labelled_passages_from_s3_to_vespa,
@@ -29,7 +30,6 @@ from flows.inference import (
     run_classifier_inference_on_batch_of_documents,
 )
 from flows.wikibase_to_s3 import wikibase_to_s3
-from flows.data_backup import data_backup
 from scripts.cloud import PROJECT_NAME, AwsEnv, generate_deployment_name
 
 MEGABYTES_PER_GIGABYTE = 1024
