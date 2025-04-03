9f8c69d2de74a997690d18ef6e1830f31a0652b6
mpjuhasz <juhaszmp@gmail.com>
2025-04-03 16:31:52 +0200

    removed argilla v1, and reference from `upload_targets_data` script

--- scripts/upload_targets_data.py
+++ scripts/upload_targets_data.py
@@ -2,19 +2,11 @@ import json
 import os
 from datetime import datetime
 
-import argilla.v1 as rg_v1
+# import argilla.v1 as rg_v1
 import huggingface_hub as hf
 import pandas as pd
 import typer
-from argilla.v1 import (
-    FeedbackDataset,
-    FeedbackRecord,
-    ResponseSchema,
-    SpanQuestion,
-    SpanValueSchema,
-    TextField,
-    ValueSchema,
-)
+
 from datasets import DatasetDict, load_dataset
 from dotenv import find_dotenv, load_dotenv
 from rich.console import Console
@@ -107,97 +99,96 @@ def main():
     df = ds["train"].to_pandas()
     assert isinstance(df, pd.DataFrame)
     console.log(f"✅ Loaded {len(df)} labelled passages from HF")
+    # NOTE: this script was run with legacy argilla v1.
+    # Since the legacy handling has been removed, this script will not work as is, but is still retained
+    # for illustration purposes.
 
     # wrangling and uploading to Argilla
-    rg_v1.init(
-        api_key=os.getenv("ARGILLA_API_KEY"), api_url=os.getenv("ARGILLA_API_URL")
-    )
+    # rg_v1.init(
+    #     api_key=os.getenv("ARGILLA_API_KEY"), api_url=os.getenv("ARGILLA_API_URL")
+    # )
 
-    workspace_name = "knowledge-graph"  # after discussing with Harrison, this is where everything should go
+    # workspace_name = "knowledge-graph"  # after discussing with Harrison, this is where everything should go
 
-    try:
-        workspace = rg_v1.Workspace.create(name=workspace_name)
-        console.log(f'✅ Created workspace "{workspace.name}"')
-    except ValueError:
-        workspace = rg_v1.Workspace.from_name(name=workspace_name)
-        console.log(f'✅ Loaded workspace "{workspace.name}"')
+    # try:
+    #     workspace = rg_v1.Workspace.create(name=workspace_name)
+    #     console.log(f'✅ Created workspace "{workspace.name}"')
+    # except ValueError:
+    #     workspace = rg_v1.Workspace.from_name(name=workspace_name)
+    #     console.log(f'✅ Loaded workspace "{workspace.name}"')
 
-    usernames = set(df["annotation_agent"].tolist())
+    # usernames = set(df["annotation_agent"].tolist())
 
-    for username in usernames:
-        try:
-            password = generate_identifier(username)
-            _ = rg_v1.User.create(
-                username=username,
-                password=password,
-                role="annotator",  # type: ignore
-            )
-            console.log(f'✅ Created user "{username}" with password "{password}"')
-        except KeyError:
-            console.log(f'✅ User "{username}" already exists')
-            _ = rg_v1.User.from_name(username)
+    # for username in usernames:
+    #     try:
+    #         password = generate_identifier(username)
+    #         _ = rg_v1.User.create(
+    #             username=username,
+    #             password=password,
+    #             role="annotator",  # type: ignore
+    #         )
+    #         console.log(f'✅ Created user "{username}" with password "{password}"')
+    #     except KeyError:
+    #         console.log(f'✅ User "{username}" already exists')
+    #         _ = rg_v1.User.from_name(username)
 
-    for concept in concepts:
-        target_labelled_passages = [
-            labelled_passage_from_row(row, concept.wikibase_id)
-            for _, row in df.iterrows()
-        ]
-        # copied this over from scripts/push_to_argilla.py
-        dataset = FeedbackDataset(
-            guidelines="Highlight the entity if it is present in the text",
-            fields=[
-                TextField(name="text", title="Text", use_markdown=True),  # type: ignore
-            ],
-            questions=[
-                SpanQuestion(  # type: ignore
-                    name="entities",
-                    labels={concept.wikibase_id: concept.preferred_label},
-                    field="text",
-                    required=True,
-                    allow_overlapping=False,
-                )
-            ],
-        )
+    # for concept in concepts:
+    #     target_labelled_passages = [
+    #         labelled_passage_from_row(row, concept.wikibase_id)
+    #         for _, row in df.iterrows()
+    #     ]
+    #     # copied this over from scripts/push_to_argilla.py
+    #     dataset = FeedbackDataset(
+    #         guidelines="Highlight the entity if it is present in the text",
+    #         fields=[
+    #             TextField(name="text", title="Text", use_markdown=True),  # type: ignore
+    #         ],
+    #         questions=[
+    #             SpanQuestion(  # type: ignore
+    #                 name="entities",
+    #                 labels={concept.wikibase_id: concept.preferred_label},
+    #                 field="text",
+    #                 required=True,
+    #                 allow_overlapping=False,
+    #             )
+    #         ],
+    #     )
 
-        records = []
+    #     records = []
 
-        for passage in target_labelled_passages:
-            record = FeedbackRecord(
-                fields={"text": passage.text},  # type: ignore
-                metadata=passage.metadata,
-                responses=[
-                    ResponseSchema(
-                        values=[
-                            {
-                                "entities": ValueSchema(
-                                    value=[
-                                        SpanValueSchema(
-                                            label=concept.wikibase_id,
-                                            start=span.start_index,
-                                            end=span.end_index,
-                                        )
-                                    ]
-                                )
-                            }
-                        ]  # type: ignore
-                    )
-                    for span in passage.spans
-                ],
-            )
-            records.append(record)
+    #     for passage in target_labelled_passages:
+    #         record = FeedbackRecord(
+    #             fields={"text": passage.text},  # type: ignore
+    #             metadata=passage.metadata,
+    #             responses=[
+    #                 ResponseSchema(
+    #                     values=[
+    #                         {
+    #                             "entities": ValueSchema(
+    #                                 value=[
+    #                                     SpanValueSchema(
+    #                                         label=concept.wikibase_id,
+    #                                         start=span.start_index,
+    #                                         end=span.end_index,
+    #                                     )
+    #                                 ]
+    #                             )
+    #                         }
+    #                     ]  # type: ignore
+    #                 )
+    #                 for span in passage.spans
+    #             ],
+    #         )
+    #         records.append(record)
 
-        dataset.add_records(records)  # type: ignore
+    #     dataset.add_records(records)  # type: ignore
 
-        # NOTE: this script was run with legacy argilla v1.
-        # Since the legacy handling has been removed, this script will not work as is, but is still retained
-        # for illustration purposes.
+    # dataset_name = concept_to_dataset_name(concept)
 
-        # dataset_name = concept_to_dataset_name(concept)
-
-        # dataset_in_argilla = dataset.push_to_argilla(
-        #     name=dataset_name, workspace=workspace_name, show_progress=False
-        # )
-        # console.log(f'✅ Created dataset for "{concept}" at {dataset_in_argilla.url}')
+    # dataset_in_argilla = dataset.push_to_argilla(
+    #     name=dataset_name, workspace=workspace_name, show_progress=False
+    # )
+    # console.log(f'✅ Created dataset for "{concept}" at {dataset_in_argilla.url}')
 
 
 if __name__ == "__main__":
