25eda53c9c2378a1625c25da9880a0571afa48b1
Mark <mark@climatepolicyradar.org>
2025-04-15 11:23:40 +0100

    Refactoring.

--- flows/inference.py
+++ flows/inference.py
@@ -12,6 +12,7 @@ from uuid import UUID
 
 import boto3
 import prefect.artifacts as artifacts
+import wandb
 from cpr_sdk.parser_models import BaseParserOutput, BlockType
 from cpr_sdk.ssm import get_aws_ssm_param
 from prefect import flow
@@ -21,8 +22,8 @@ from prefect.deployments import run_deployment
 from prefect.logging import get_run_logger
 from prefect.task_runners import ConcurrentTaskRunner
 from pydantic import SecretStr
+from wandb.sdk.wandb_run import Run
 
-import wandb
 from flows.utils import (
     SlackNotify,
     filter_non_english_language_file_stems,
@@ -40,7 +41,6 @@ from scripts.update_classifier_spec import parse_spec_file
 from src.classifier import Classifier
 from src.labelled_passage import LabelledPassage
 from src.span import Span
-from wandb.sdk.wandb_run import Run
 
 # The "parent" AKA the higher level flows that do multiple things
 PARENT_TIMEOUT_S: int = int(timedelta(hours=5).total_seconds())
