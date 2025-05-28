7fd696cbd49cb41cfae2c5672e48757ec16674ef
Mark <mark@climatepolicyradar.org>
2025-05-28 14:45:24 +0100

    Refactoring.

--- tests/flows/test_index_from_aggregate_results.py
+++ tests/flows/test_index_from_aggregate_results.py
@@ -6,10 +6,21 @@ def test_index_from_aggregated_inference_results(
 ) -> None:
     """Test that we loaded the inference results from the mock bucket."""
 
-    # TODO: Remove when implementing functionality.
+    # TODO: The aggregate inference results function returns a unique s3 sub prefix,
+    # move this functionality into a parent flow that takes the prefix as a parameter
+    # and passes into the subflow once we have developed it.
     response = mock_s3_client.list_objects_v2(
         Bucket=mock_bucket, Prefix=s3_prefix_inference_results
     )
-    assert "Contents" in response and len(response["Contents"]) > 0
+    assert "Contents" in response, "No objects found in bucket"
+    assert len(response["Contents"]) > 0, "Empty response contents"
 
-    # TODO: Run the flow to index the results.
+    file_keys = [
+        obj["Key"] for obj in response["Contents"] if obj["Key"].endswith(".json")
+    ]
+    assert file_keys, "No JSON files found in bucket"
+
+    for file_key in file_keys:
+        response = mock_s3_client.get_object(Bucket=mock_bucket, Key=file_key)
+
+        # TODO: Run the flow to index the results.
