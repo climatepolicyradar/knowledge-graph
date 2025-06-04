e09ad1a1d0c69ecc8aef4dfa4b410db9c0a5a4c2
mpjuhasz <juhaszmp@gmail.com>
2025-06-04 16:00:44 +0100

    typo

--- tests/flows/test_deindex.py
+++ tests/flows/test_deindex.py
@@ -354,7 +354,7 @@ def test_update_s3_with_some_successes(
         )
     ) == [
         '{"id":"lp1","text":"once upon a time","source":null,"spans":[{"text":"upon","start_index":0,"end_index":1,"concept_id":"Q456","labellers":[],"timestamps":[],"confidence":null,"id":"bpp4juku","labelled_text":"u"}],"metadata":{}}',
-        '{"id":"lp2","text":"lorem ipsum",,"source":null,"spans":[],"metadata":{}}',
+        '{"id":"lp2","text":"lorem ipsum","source":null,"spans":[],"metadata":{}}',
     ]
     assert json.loads(
         _s3_object_read_text(
@@ -544,7 +544,9 @@ async def test_run_partial_updates_of_concepts_for_document_passages(
 ):
     document_import_id_remove: DocumentImportId = "CCLW.executive.10014.4470"
 
-    document_object_uri_remove: DocumentObjectUri = f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q760/v4/{document_import_id_remove}.json"
+    document_object_uri_remove: DocumentObjectUri = (
+        f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q760/v4/{document_import_id_remove}.json"
+    )
     document_importer_remove: DocumentImporter = (
         document_import_id_remove,
         document_object_uri_remove,
@@ -552,7 +554,9 @@ async def test_run_partial_updates_of_concepts_for_document_passages(
 
     document_import_id_keep: DocumentImportId = "CCLW.executive.4934.1571"
 
-    document_object_uri_keep: DocumentObjectUri = f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q787/v4/{document_import_id_keep}.json"
+    document_object_uri_keep: DocumentObjectUri = (
+        f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q787/v4/{document_import_id_keep}.json"
+    )
     _document_importer_keep: DocumentImporter = (
         document_import_id_keep,
         document_object_uri_keep,
@@ -834,13 +838,21 @@ async def test_deindex_labelled_passages_from_s3_to_vespa(
 
     document_import_id_remove: DocumentImportId = "CCLW.executive.10014.4470"
 
-    document_object_uri_remove: DocumentObjectUri = f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q760/{primary_alias}/{document_import_id_remove}.json"
-    document_object_uri_remove_cleanup_1: DocumentObjectUri = f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q760/{cleanup_1_alias}/{document_import_id_remove}.json"
-    document_object_uri_remove_cleanup_2: DocumentObjectUri = f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q760/{cleanup_2_alias}/{document_import_id_remove}.json"
+    document_object_uri_remove: DocumentObjectUri = (
+        f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q760/{primary_alias}/{document_import_id_remove}.json"
+    )
+    document_object_uri_remove_cleanup_1: DocumentObjectUri = (
+        f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q760/{cleanup_1_alias}/{document_import_id_remove}.json"
+    )
+    document_object_uri_remove_cleanup_2: DocumentObjectUri = (
+        f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q760/{cleanup_2_alias}/{document_import_id_remove}.json"
+    )
 
     document_import_id_keep: DocumentImportId = "CCLW.executive.4934.1571"
 
-    document_object_uri_keep: DocumentObjectUri = f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q787/{primary_alias}/{document_import_id_keep}.json"
+    document_object_uri_keep: DocumentObjectUri = (
+        f"s3://{mock_bucket}/{DOCUMENT_TARGET_PREFIX_DEFAULT}/Q787/{primary_alias}/{document_import_id_keep}.json"
+    )
     _document_importer_keep: DocumentImporter = (
         document_import_id_keep,
         document_object_uri_keep,
