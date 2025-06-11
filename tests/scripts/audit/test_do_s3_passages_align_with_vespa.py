4ca01bebd40d1e9d9a343dffcad51c6c3afbaf6f
Mark <mark@climatepolicyradar.org>
2025-06-11 15:41:50 +0100

    Removing some comments.

--- tests/scripts/audit/test_do_s3_passages_align_with_vespa.py
+++ tests/scripts/audit/test_do_s3_passages_align_with_vespa.py
@@ -11,16 +11,13 @@ def test_get_vespa_passage_counts(
 ) -> None:
     """Test the get_vespa_passage_counts function."""
 
-    # Define test data
     expected_counts = {
         DocumentImportId("CCLW.executive.10014.4470"): 1830,
         DocumentImportId("CCLW.executive.4934.1571"): 135,
     }
 
-    # Get actual results
     vespa_passage_counts = get_vespa_passage_counts(vespa=local_vespa_search_adapter)
 
-    # Assertions
     assert vespa_passage_counts == expected_counts, (
         "Passage counts don't match expected values"
     )
