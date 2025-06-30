f2a12ad3c3381a51e3ca03447e2442e602cb71fb
harrisonpim <hello@harrisonpim.com>
2025-06-30 10:18:21 +0100

    delete the unused _distribute_labelling_projects method

--- src/labelling.py
+++ src/labelling.py
@@ -1,8 +1,7 @@
 import os
 import uuid
 from datetime import datetime
-from itertools import cycle
-from typing import Generator, Optional
+from typing import Optional
 
 from argilla import (
     Argilla,
@@ -254,33 +253,6 @@ class ArgillaSession:
                 labelled_passages, min_timestamp, max_timestamp
             )
         return labelled_passages
-
-    @staticmethod
-    def _distribute_labelling_projects(
-        datasets: list, labellers: list[str], min_labellers: int = 2
-    ) -> Generator[tuple[Dataset, str], None, None]:
-        """
-        Distribute labelling projects to labellers.
-
-        For efficient labelling, tasks should be distributed such that each dataset is
-        labelled by at least `min_labellers` labellers, and each labeller is assigned to a
-        minimal number of datasets.
-
-        :param list[] datasets: datasets to distribute among labellers
-        :param list[str] labellers: list of labellers
-        :param int min_labellers: minimum number of labellers per dataset, defaults to 2
-        :return Generator[tuple[FeedbackDataset, str], None, None]: a generator of tuples containing
-            the dataset and the labeller assigned to it
-        """
-        if len(labellers) < min_labellers:
-            raise ValueError(
-                "number of items in labellers must be greater than or equal to min_labellers"
-            )
-
-        labeller_cycle = cycle(labellers)
-        for dataset in datasets:
-            for _ in range(min_labellers):
-                yield dataset, next(labeller_cycle)
 
     def combine_datasets(self, *datasets: Dataset) -> Dataset:
         """
