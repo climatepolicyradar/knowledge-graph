2d7d8da2c60acaa06348c9f7bef7f412fdcc84d0
olaughter <fred@climatepolicyradar.org>
2025-07-29 15:49:00 +0100

    refac: simplify local classifier saving
    Spotted some duplication & spread out code that I was able to bring
    together related to how we save classifiers

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,14 +1,10 @@
 from pathlib import Path
 
-from scripts.config import processed_data_dir
+from scripts.config import classifier_dir
 from src.classifier import Classifier
-from src.concept import Concept
 
 
-def get_local_classifier_path(concept: Concept, classifier: Classifier) -> Path:
+def get_local_classifier_path(classifier: Classifier) -> Path:
     return (
-        processed_data_dir
-        / Path("classifiers")
-        / str(concept.wikibase_id)
-        / f"{classifier.id}.pickle"
+        classifier_dir / str(classifier.concept.wikibase_id) / f"{classifier.id}.pickle"
     )
