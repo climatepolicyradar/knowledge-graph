dc0da375631d3a766d129f66a03342af53245f81
olaughter <fred@climatepolicyradar.org>
2025-08-06 11:03:05 +0100

    test: add coverage for demote
    As the test for demote was missing

---
+++ tests/test_demote.py
@@ -0,0 +1,44 @@
+import os
+from unittest.mock import Mock, patch
+
+import pytest
+import typer
+from moto import mock_aws
+
+from scripts.cloud import AwsEnv
+from scripts.demote import main
+
+
+@pytest.mark.parametrize(
+    ("wikibase_id", "aws_env", "logged_in", "expected_exception"),
+    [
+        ("Q123", AwsEnv.labs, True, None),
+        ("Q456", AwsEnv.staging, True, None),
+        ("Q789", AwsEnv.labs, False, typer.BadParameter),
+    ],
+)
+@mock_aws
+def test_main(wikibase_id, aws_env, logged_in, expected_exception, monkeypatch):
+    os.environ["USE_AWS_PROFILES"] = "false"
+
+    # Mock wandb artifact
+    artifact_mock = Mock()
+    artifact_mock.aliases = Mock()
+    artifact_mock.aliases.remove = Mock()
+    artifact_mock.save = Mock()
+
+    # Mock wandb API
+    api_mock = Mock()
+    api_mock.artifact.return_value = artifact_mock
+
+    monkeypatch.setattr("wandb.Api", lambda: api_mock)
+
+    with patch("scripts.demote.is_logged_in", return_value=logged_in):
+        if expected_exception:
+            with pytest.raises(expected_exception):
+                main(wikibase_id=wikibase_id, aws_env=aws_env)
+                api_mock.artifact.assert_not_called()
+
+        else:
+            main(wikibase_id=wikibase_id, aws_env=aws_env)
+            artifact_mock.aliases.remove.assert_called_once_with(aws_env.value)
