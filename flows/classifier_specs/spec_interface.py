53e64cce763e7129d54a8ddc0a7fb61b22b16d84
olaughter <fred@climatepolicyradar.org>
2025-08-27 12:59:31 +0100

    fix: Version serialisation for pydantic 2
    Version was still using pydantic 1 syntax for serialisation. This had
    led to classifier specs storing an integer instead of a string for
    versions and that in turn meant they could not be read as they failed
    validation.

--- flows/classifier_specs/spec_interface.py
+++ flows/classifier_specs/spec_interface.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import Optional
 
 import yaml
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_serializer
 
 from scripts.cloud import AwsEnv
 from scripts.utils import DontRunOnEnum
@@ -44,6 +44,11 @@ class ClassifierSpec(BaseModel):
         description="A list of `source`'s that will be filtered out in inference.",
         default=None,
     )
+
+    @field_serializer("wandb_registry_version")
+    def serialize_version(self, value: Version) -> str:
+        """Serialize Version as string."""
+        return str(value)
 
     def __hash__(self):
         """Make ClassifierSpec hashable for use in sets and as dict keys."""
