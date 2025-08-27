e0579e45f87d3cca0a4c6e4b4316bac160b606c7
olaughter <fred@climatepolicyradar.org>
2025-08-27 11:33:08 +0100

    feat: enable whole spec metadata updates
    To facilitate metadata updates that apply across the whole spec file for
    an environment.

--- scripts/classifier_metadata.py
+++ scripts/classifier_metadata.py
@@ -8,6 +8,7 @@ import wandb
 import wandb.apis.public.api
 from rich.console import Console
 
+from flows.classifier_specs.spec_interface import load_classifier_specs
 from scripts.cloud import AwsEnv
 from scripts.config import WANDB_ENTITY
 from scripts.update_classifier_spec import get_all_available_classifiers
@@ -25,7 +26,42 @@ console = Console()
 
 
 @app.command()
-def main(
+def update_entire_env(
+    clear_dont_run_on: Annotated[
+        bool, typer.Option(help="Remove all existing items from dont_run_on")
+    ] = False,
+    add_dont_run_on: Annotated[
+        list[DontRunOnEnum] | None,
+        typer.Option(help="Adds a single item to the metadata."),
+    ] = None,
+    aws_env: Annotated[
+        AwsEnv,
+        typer.Option(help="AWS environment the classifier belongs to"),
+    ] = AwsEnv.labs,
+    update_specs: Annotated[
+        bool,
+        typer.Option(
+            help="Also update the classifier specs for the environment following changes"
+        ),
+    ] = True,
+):
+    specs = load_classifier_specs(aws_env)
+    for spec in specs:
+        update(
+            wikibase_id=spec.wikibase_id,
+            classifier_id=spec.classifier_id,
+            clear_dont_run_on=clear_dont_run_on,
+            add_dont_run_on=add_dont_run_on,
+            aws_env=aws_env,
+            update_specs=False,  # since we'll only update once all are done
+        )
+
+    if update_specs:
+        get_all_available_classifiers([aws_env])
+
+
+@app.command()
+def update(
     wikibase_id: Annotated[
         WikibaseID,
         typer.Option(
@@ -77,6 +113,11 @@ def main(
 
         if clear_dont_run_on:
             console.log(f"Clearing existing `dont_run_on` metadata from {artifact_id}")
+            if add_dont_run_on:
+                console.log(
+                    "`add_dont_run_on` is set, so values will be fully refreshed"
+                )
+
             artifact.metadata.pop("dont_run_on", None)
 
         if add_dont_run_on:
