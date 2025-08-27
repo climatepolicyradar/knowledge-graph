d6f749e70c8a355b9d80cbd20a1f10db07fc2b74
Mark <mark@climatepolicyradar.org>
2025-08-27 14:44:24 +0100

    Updating the full pipeline to use the v2 classifier specs.

--- flows/full_pipeline.py
+++ flows/full_pipeline.py
@@ -13,6 +13,7 @@ from flows.aggregate import (
 from flows.boundary import (
     DEFAULT_DOCUMENTS_BATCH_SIZE as INDEXING_DEFAULT_DOCUMENTS_BATCH_SIZE,
 )
+from flows.classifier_specs.spec_interface import ClassifierSpec
 from flows.config import Config
 from flows.index import (
     DEFAULT_INDEXER_CONCURRENCY_LIMIT,
@@ -27,7 +28,6 @@ from flows.inference import (
     inference,
 )
 from flows.utils import DocumentImportId, Fault
-from scripts.cloud import ClassifierSpec
 
 
 # pyright: reportCallIssue=false, reportGeneralTypeIssues=false
