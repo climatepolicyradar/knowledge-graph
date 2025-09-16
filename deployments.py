ead02756f7925a3dc91d24a024dca43f4f7a5997
olaughter <fred@climatepolicyradar.org>
2025-09-16 12:03:13 +0100

    chore: add deployment for train_on_gpu

--- deployments.py
+++ deployments.py
@@ -24,6 +24,7 @@ from flows.inference import (
     inference_batch_of_documents_cpu,
     inference_batch_of_documents_gpu,
 )
+from flows.train import train_on_gpu
 from flows.update_neo4j import update_neo4j
 from flows.utils import JsonDict
 from flows.wikibase_to_s3 import wikibase_to_s3
@@ -138,6 +139,14 @@ def create_deployment(
         push=False,
     )
 
+
+# Train
+create_deployment(
+    flow=train_on_gpu,
+    description="Train concept classifiers with GPU compute",
+    gpu=True,
+    flow_variables={},
+)
 
 # Inference
 
