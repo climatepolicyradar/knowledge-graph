7870182f4a95c91da7542592a902e9f49605e9e5
Harrison Pim <hello@harrisonpim.com>
2025-09-17 12:49:13 +0100

    fix typo in cron schedule (#692)

--- deployments.py
+++ deployments.py
@@ -255,7 +255,7 @@ create_deployment(
     flow=update_neo4j,
     description="Refresh Neo4j with the latest concept graph",
     env_schedules={
-        AwsEnv.labs: "0 3 * * * MON-THU",
+        AwsEnv.labs: "0 3 * * MON-THU",
     },
 )
 
