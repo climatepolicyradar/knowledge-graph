6494a8de7f7fd4a8f0650246bf22ee4ae3c323a5
Kerry Parker <kerry.parker@climatepolicyradar.org>
2025-11-04 10:23:20 +0000

    add get_classifier_profiles test

---
+++ tests/flows/test_classifiers_profiles.py
@@ -0,0 +1,84 @@
+from unittest.mock import AsyncMock
+
+import pytest
+
+from flows.classifiers_profiles import get_classifier_profiles
+from knowledge_graph.classifiers_profiles import ClassifiersProfile, Profile
+from knowledge_graph.concept import Concept
+from knowledge_graph.identifiers import ClassifierID, WikibaseID
+from knowledge_graph.wikibase import StatementRank, WikibaseSession
+
+
+def test_add_classifier_profile():
+    pass
+
+
+def test_remove_classifier_profile():
+    pass
+
+
+def test_update_classifier_profile():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_get_classifier_profiles():
+    # mock concepts
+    list_concepts = [
+        Concept(wikibase_id="Q123", preferred_label="Concept 123"),
+        Concept(wikibase_id="Q100", preferred_label="Concept 100"),
+        Concept(wikibase_id="Q999", preferred_label="Concept 999"),
+        Concept(wikibase_id="Q200", preferred_label="Concept 200"),
+    ]
+
+    # mock response from wikibase.get_classifier_ids_async
+    mock_wikibase = AsyncMock(spec=WikibaseSession)
+
+    # 1 success, 3 failures
+    mock_wikibase.get_classifier_ids_async.side_effect = [
+        # Q123: success
+        [
+            (StatementRank.PREFERRED, ClassifierID("aaaa2222")),
+            (StatementRank.DEPRECATED, ClassifierID("yyyy9999")),
+        ],
+        # Q100: failure
+        Exception("Failed to fetch classifier profiles for Q100"),
+        # Q999: fail validation - 2 primary profiles
+        [
+            (StatementRank.PREFERRED, ClassifierID("bbbb3333")),
+            (StatementRank.PREFERRED, ClassifierID("cccc4444")),
+        ],
+        # Q200: fail validation - same classifier ID in 2 profiles
+        [
+            (StatementRank.PREFERRED, ClassifierID("xyzz2345")),
+            (StatementRank.DEPRECATED, ClassifierID("xyzz2345")),
+        ],
+    ]
+
+    # Call the function under test
+    classifier_profiles, validation_errors = await get_classifier_profiles(
+        wikibase=mock_wikibase, concepts=list_concepts
+    )
+
+    # assert successful profiles
+    assert len(classifier_profiles) == 2
+    assert classifier_profiles[0] == ClassifiersProfile(
+        wikibase_id=WikibaseID("Q123"),
+        classifier_id=ClassifierID("aaaa2222"),
+        classifier_profile=Profile.PRIMARY,
+    )
+    assert classifier_profiles[1] == ClassifiersProfile(
+        wikibase_id=WikibaseID("Q123"),
+        classifier_id=ClassifierID("yyyy9999"),
+        classifier_profile=Profile.RETIRED,
+    )
+
+    # assert validation errors
+    assert len(validation_errors) == 3
+    assert validation_errors[0] == {
+        "wikibase_id": "Q100",
+        "Error": "Failed to fetch classifier profiles for Q100",
+    }
+
+    # check mocked method called
+    assert mock_wikibase.get_classifier_ids_async.call_count == 4
