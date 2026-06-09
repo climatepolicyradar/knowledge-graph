67208f37665d6ab145f9d3c0e9b5517c585571b9
Kalyan Dutia <20212179+kdutia@users.noreply.github.com>
2026-06-09 16:48:57 +0100

    manage feather files bucket in pulumi

---
+++ infra/README.md
@@ -0,0 +1,5 @@
+# AWS S3 bucket infra-as-code
+
+The code in this repo manages the bucket `s3://cpr-kg-feather-files` which exists in production and has cross-account access to labs.
+
+Vibe checker infra on AWS labs is managed separately in `vibe-checker/infra`.
---
+++ infra/__main__.py
@@ -0,0 +1,58 @@
+import pulumi
+import pulumi_aws as aws
+
+config = pulumi.Config()
+labs_aws_account_id = config.get("labs_aws_account_id")
+
+production_knowledge_graph_feather_files_bucket = aws.s3.Bucket(
+    "production-knowledge-graph-feather-files-bucket",
+    bucket="cpr-kg-feather-files",
+    grants=[
+        aws.s3.BucketGrantArgs(
+            id="0fedc730a2af259d90402b1197e87cf40c4014a20851f540cac4269c0156abb9",
+            permissions=["FULL_CONTROL"],
+            type="CanonicalUser",
+        )
+    ],
+    region="eu-west-1",
+    request_payer="BucketOwner",
+    server_side_encryption_configuration=aws.s3.BucketServerSideEncryptionConfigurationArgs(
+        rule=aws.s3.BucketServerSideEncryptionConfigurationRuleArgs(
+            apply_server_side_encryption_by_default=aws.s3.BucketServerSideEncryptionConfigurationRuleApplyServerSideEncryptionByDefaultArgs(
+                sse_algorithm="AES256",
+            ),
+            bucket_key_enabled=True,
+        ),
+    ),
+    opts=pulumi.ResourceOptions(protect=True),
+)
+
+# Grant the labs AWS account read-only access to the feather files bucket
+labs_cross_account_policy = aws.iam.get_policy_document(
+    statements=[
+        aws.iam.GetPolicyDocumentStatementArgs(
+            sid="LabsCrossAccountReadOnly",
+            effect="Allow",
+            principals=[
+                aws.iam.GetPolicyDocumentStatementPrincipalArgs(
+                    type="AWS",
+                    identifiers=[f"arn:aws:iam::{labs_aws_account_id}:root"],
+                )
+            ],
+            actions=[
+                "s3:GetObject",
+                "s3:ListBucket",
+            ],
+            resources=[
+                "arn:aws:s3:::cpr-kg-feather-files",
+                "arn:aws:s3:::cpr-kg-feather-files/*",
+            ],
+        ),
+    ],
+)
+
+production_knowledge_graph_feather_files_bucket_policy = aws.s3.BucketPolicy(
+    "production-knowledge-graph-feather-files-bucket-policy",
+    bucket=production_knowledge_graph_feather_files_bucket.id,
+    policy=labs_cross_account_policy.json,
+)
